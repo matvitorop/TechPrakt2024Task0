27830bda4b9fdf00877506bfcb65f12cd0613b22
MatviiTorop <ipz224_tmd@student.ztu.edu.ua>
2024-01-28 23:03:41 +0200

    Створено обчислювач суми чисел, додано gitignore

---
+++ Adding_numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Введіть перше число:");
+
+if (double.TryParse(Console.ReadLine(), out double number1))
+{
+    Console.WriteLine("Введіть друге число:");
+    if (double.TryParse(Console.ReadLine(), out double number2))
+    {
+        double sum = number1 + number2;
+        Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює: {sum}");
+    }
+    else
+    {
+        Console.WriteLine("Невірний формат другого числа.");
+    }
+}
+else
+{
+    Console.WriteLine("Невірний формат першого числа.");
+}
